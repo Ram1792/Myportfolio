5e665d4e4fe9547373aeb37876a51302d9ff1a47
Ram1792 <ramadurgaprasadkarri@gmail.com>
2024-09-25 21:08:38 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+// let theme=document.getElementById("menu-icon2");
+// let ones=document.querySelector(".one");
+// let btn1=document.querySelector(".btn");
+// let spans=document.querySelectorAll("span");
+// let col="darkorange"
+
+
+// theme.addEventListener("click",()=>{
+//     if(col=="darkorange"){
+//         col="green"
+//         theme.style.color="red";  
+//         ones.style.backgroundColor="red";  
+//         btn1.style.backgroundColor="red";  
+//         spans.style.color="red" ;
+//   }else{
+//     col="darkorange"
+//     theme.style.color="green";  
+//     ones.style.backgroundColor="green";  
+//     btn1.style.backgroundColor="green";  
+//   }
+// })
